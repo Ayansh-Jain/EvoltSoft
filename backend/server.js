636a01f3417dbec1184791efa44f7a20dd21b54b
Ayansh-Jain <ayanshjain2006@gmail.com>
2025-06-03 18:54:49 +0530

    5

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ app.use(cors());
 // Routes
 app.use('/auth', authRoutes);
 app.use('/stations', stationRoutes);
-
+app.get('/', (req, res) => {
+  res.send('EvoltSoft backend is running!');
+});
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
